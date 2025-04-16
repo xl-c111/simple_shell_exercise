a24c80ee82ebbfee6ffab1f788e2989d8af85869
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-16 12:47:48 +1000

    Update prompt.c
    ssize_t getline(char **lineptr, size_t *n, FILE *stream)
    &line == char **
    &n == size_t *

--- prompt.c
+++ prompt.c
@@ -6,25 +6,30 @@
  *        prints it on the next line
  * 
  * Return: Always 0
+ * Description: ssize_t getline(char **lineptr, size_t *n, FILE *stream)
+ *             - @line： ptr to a string, when we pass to getline, we use &line(type of char**)
+ *                       because getline wants to allocate and reallocate memory and change the value of line
+ *             - @n: to store the allocated buffer size. If *line is NULL or not large enough, getline will use realloc to resize it
+ *             - @stream: the standard input stream, getline reads from this stream, so it will wait user to type sth and hit enter
  */
 
 int main (void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
+	char *line = NULL;         /* declare a ptr to string, initialize it to NULL */
+	size_t len = 0;            /* declare a variable len to store the allocated buffer size for getline */
+	ssize_t read;              /* declare a signed size to store the number of chars read by getline  */
 
 	while (1)
 	{
 		printf("$ ");
-		read = getline(&line, &len, stdin);
+		read = getline(&line, &len, stdin);   /* &line == char**; &line == size_t * */
 
 		if (read == -1)
 		{
 			printf("\n");
 			break;
 		}
-		printf("%s", line);
+		printf("%s", line);     /* getline() reads input including the new line, this will already include \n in the string */
 	}
 	free(line);
 	return (0); 
