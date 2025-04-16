fc19ecd4d93f0970535b137f073f8c5395f90b47
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-16 15:00:08 +1000

    Update split_str.c
    - strtok(str, delim): first call, pass the original string to tokenize
    - strtok(NULL, delim): sebsuquent calls, continue tokenizing from where the last call left off

--- split_str.c
+++ split_str.c
@@ -6,13 +6,21 @@
  * split_string - splits a string
  * @str: a pointer to string
  *
- * Return: an array of each word of the string
+ * Return: a pointer to an array of strings (tokens)
+ * Description: - char *strtok(char *str, const char *delim);
+ *                       - breaks a string into a series of tokens using the characters in delim as delimiters
+ *                       - return a pointer to the next token, or NULL if no more token found
+ *              - store each token into array(tokens[i++] - token;)
+ *              - realloc if the array is full (tokens = realloc(tokens, sizeof(char *) * bufsize);)
+ *              - use a loop with strtok(NULL, ...) to get all tokens until NULL is returned
+ *                       - strtok(str, delim): first call, pass the original string to tokenize
+ *                       - strtok(NULL, delim): sebsuquent calls, continue tokenizing from where the last call left off 
  */
 char **split_string(char *str)
 {
-	int i = 0;
-	int bufsize = 64;
-	char **tokens = malloc(sizeof(char *) * bufsize);
+	int i = 0;                   /* index for storing tokens */
+	int bufsize = 64;            /* initial size of tokens array */
+	char **tokens = malloc(sizeof(char *) * bufsize);  /* allocate memory for an array  of char *(each of which points to a token) */
 	char *token;
 
 	if (tokens == NULL)
@@ -20,25 +28,25 @@ char **split_string(char *str)
 		perror("malloc failed");
 		exit(EXIT_FAILURE);
 	}
-	
+	/* use strtok to extract the first token from str, using space, tab, carriage return, newline as delimiters */
 	token = strtok(str, " \t\r\n");
-	while (token != NULL)
+	while (token != NULL)                /* loop as long as strtok returning valid tokens */
 	{
-		tokens[i++] = token;
+		tokens[i++] = token;         /* store the current token in the array and increment the index */
 
-		if (i >= bufsize)
+		if (i >= bufsize)     /* if the array is full, increase the bufsize and reallocate memory */
 		{
 			bufsize += 64;
+		        tokens = realloc(tokens, sizeof(char *) * bufsize);
+		        if (tokens == NULL)
+		        {
+				perror("realloc failed");
+				exit(EXIT_FAILURE);
+		        }
 		}
-		tokens = realloc(tokens, sizeof(char *) * bufsize);
-		if (tokens == NULL)
-		{
-			perror("realloc failed");
-			exit(EXIT_FAILURE);
-		}
-		token = strtok(NULL, " \t\r\n");
+		token = strtok(NULL, " \t\r\n");     /* pass NULL tells continue from where you left off */
 	}
-	tokens[i] = NULL;
+	tokens[i] = NULL;     /* Null-terminate the array to indicate the end of tokens */
 	return (tokens);
 }
 int main()
