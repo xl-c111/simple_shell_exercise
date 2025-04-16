df005cbefe586e10f473dd1be6ce0c991e281a6a
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-16 23:20:34 +1000

    Update exec.c
    char *argv[] = {"/bin/ls", "-l", "/usr/", NULL} equals to run the shell command /bin/ls -l /usr/

--- exec.c
+++ exec.c
@@ -5,10 +5,14 @@
  * main - execve example
  *
  * Return: Always 0.
+ * Description: char *argv[] = {"/bin/ls", "-l", "/usr/", NULL}; 
+ *              - define an array of strings that simulates the command-line argument passed to program, 
+ *                it equals to run the shell command /bin/ls -l /usr/
+ *              - in execve(argv[0], argv, NULL), this array is passed as argv
  */
 int main(void)
-{
-	char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
+{     
+	char *argv[] = {"/bin/ls", "-l", "/usr/", NULL}; 
 	
 	printf("Before execve\n");
 	
@@ -16,6 +20,6 @@ int main(void)
 	{
 		perror("Error:");
 	}
-	printf("After execve\n");
+	printf("After execve\n");       /* this line only runs when execve fails */
 	return (0);
 }
