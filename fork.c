cf2cc055e8b90e4ecdbe15d39991a69c8e7d7a1f
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-17 00:02:19 +1000

    Update fork.c
    getpid() returns the PID of current process
    
    fork() is used to create a new process
    - Positive value: PID of the child process, returned in the parent process

--- fork.c
+++ fork.c
@@ -20,8 +20,8 @@
  */
 int main(void)
 {
-	pid_t my_pid;
-	pid_t child_pid;
+	pid_t my_pid;        /* store the PID of current process */
+	pid_t child_pid;     /* store the return value from fork() */
 	
 	child_pid = fork();
 	if (child_pid == -1)
@@ -30,7 +30,7 @@ int main(void)
 		return (1);
 	}
 	
-	my_pid = getpid();
+	my_pid = getpid();         /* get the PID of the current process, whether father or child */
 	printf("My pid is %u\n", my_pid);
 	
 	if (child_pid == 0)
