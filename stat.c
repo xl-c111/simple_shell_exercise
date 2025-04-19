3619168f85ed514fcd5e2b4743ff99f0211974d0
XIA0LING CUI <10430@holbertonstudents.com>
2025-04-19 19:17:54 +1000

    Update stat.c
    int stat(const char *pathname, struct stat *statbuf)
    - use a while loop to iterate all command-line arguments: while (av[i])
    - use stat() to check the status of file path
    if (stat(av[i], &st) == 0)

--- stat.c
+++ stat.c
@@ -4,13 +4,15 @@
 #include <unistd.h>
 
 /**
- * main - stat example
- *
- * Return: Always 0.
+ * main - use stat to check whether each given file path exists
+ * @ac: the number of arguments
+ * @av: an array of strings(char pointers), one for each argument
+ *      (av[0] is always the program name, command line arguments start from av[1])
+ * Return: Always 0, 1 if no file path provided
  * 
  * Description: - input validation if (ac < 2);
- *              - argument iteration while (av[i]); i++;
- *              _ file existence checking using stat()
+ *              - argument iteration while (av[i]); i++(i set to 1 to skip the program name);
+ *              - file existence checking using stat()
  *                       int stat(const char *pathname, struct stat *statbuf){
  *                           off_t     st_size;
  *                           mode_t    st_mode;
@@ -24,16 +26,16 @@
  */
 int main(int ac, char **av)
 {
-	unsigned int i;       /* used to iterate through command-line arguments */
+	unsigned int i;       /* used to iterate through command-line arguments, start from av[1] */
 	struct stat st;       /* a variable st of type struct stat to store the file information */
 	
-	if (ac < 2)           /* check if the argument number is fewer than 2, means there is only argv[0] = program name */
+	if (ac < 2)           /* check if the argument number is fewer than 2, means there is only av[0] = program name */
 	{
 		printf("Usage: %s path_to_file ...\n", av[0]);
 		return (1);
 	}
 	
-	i = 1;                /* initialize i to 1 to start checking from the first file list */
+	i = 1;                 /* set i to 1 to skip the program name and start checking from the first file path */
 	while (av[i])
 	{
 		printf("%s:", av[i]);               /* prints the file path being checked */
